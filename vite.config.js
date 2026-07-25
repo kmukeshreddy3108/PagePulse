8f43f3d2ca7706cd44213203b06559c05a6aa201
Kathi Mukesh Reddy <kathimukeshreddy@gmail.com>
2026-07-25 21:30:21 +0530

    Fix project build and file structure for Render and Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        host: "0.0.0.0",
-    },
+  plugins: [react()],
+  publicDir: "frontend/public",
+  server: {
+    port: Number(process.env.PORT) || 3000,
+    host: "0.0.0.0",
+  },
 });
